8a5a22f5d77d30cec5ccb4aae3b8b5b00b91ec15
yutoutang <2625962609@qq.com>
2021-04-13 16:15:19 +0800

    split CmakeLists

---
+++ examples/restful/SqlConnPool.cpp
@@ -0,0 +1,80 @@
+//
+// Created by txy on 2021/1/5.
+//
+
+#include "SqlConnPool.h"
+
+using namespace std;
+
+ym::SqlConnPool::SqlConnPool() : freeCount_(0), useCount_(0){}
+
+ym::SqlConnPool::~SqlConnPool() {
+    closeConn();
+}
+
+
+void ym::SqlConnPool::init(const char *host, unsigned int port,
+                           const char *user, const char *pwd, const char *dbName,
+                           int connSize = 10) {
+    for (int i = 0; i < connSize; ++i) {
+        MYSQL *sql = nullptr;
+        sql = mysql_init(sql);
+        if (mysql_real_connect(sql, host, user, pwd, dbName, port, nullptr, 0)){
+            connQ_.push(sql);
+        } else {
+            LOG_ERROR("sql conn error");
+        }
+        freeCount_++;
+    }
+    maxConnSize_ = connSize;
+}
+
+ym::SqlConnPool *ym::SqlConnPool::instance() {
+    static SqlConnPool sqlConnPool;
+    return &sqlConnPool;
+}
+
+// 从连接池中取出
+MYSQL *ym::SqlConnPool::getMysqlConn() {
+    MYSQL *sql = nullptr;
+    if (connQ_.empty()){
+        LOG_WARN("sqlConnPoll is empty");
+        return nullptr;
+    }
+    {
+        std::unique_lock<std::mutex> lck(mutex_);
+        useCount_++;
+        while (useCount_ > maxConnSize_){
+            cond_.wait(lck);
+        }
+        sql = connQ_.front();
+        connQ_.pop();
+    }
+    return sql;
+}
+
+int ym::SqlConnPool::getFreeCount() {
+    // 读
+    std::lock_guard<std::mutex> lck(mutex_);
+    return freeCount_;
+}
+
+void ym::SqlConnPool::closeConn() {
+    MYSQL *sql = nullptr; // 写
+    std::lock_guard<std::mutex> lck(mutex_);
+    while (connQ_.empty()){
+        sql = connQ_.front();
+        mysql_close(sql);
+        connQ_.pop();
+    }
+    mysql_library_end;
+}
+
+// sql 空闲了进入连接池中
+void ym::SqlConnPool::freeConn(MYSQL *mysql) {
+    assert(mysql); // 写
+    std::lock_guard<std::mutex> lck(mutex_);
+    connQ_.push(mysql);
+    useCount_--;
+    cond_.notify_one();
+}
---
+++ examples/restful/SqlConnPool.h
@@ -0,0 +1,67 @@
+//
+// Created by txy on 2021/1/5.
+// sql 池封装
+#ifndef YM_SERVER_SQLCONNPOOL_H
+#define YM_SERVER_SQLCONNPOOL_H
+#include "mysql.h"
+#include "string"
+#include "queue"
+#include "mutex"
+#include "thread"
+#include "condition_variable"
+
+
+#include "../log/Log.h"
+
+
+class SqlConnPool {
+public:
+    void init(const char* host, unsigned int port, const char* user, const char* pwd,
+              const char* dbName, int connSize);
+
+    static SqlConnPool* instance(); // 静态 instance，私有构造，通过 class::instance()->init 初始化
+
+    MYSQL* getMysqlConn();
+
+    int getFreeCount();
+
+    void closeConn();
+    void freeConn(MYSQL *mysql);
+private:
+    SqlConnPool();
+    ~SqlConnPool();
+
+    int maxConnSize_;
+    int freeCount_;
+    int useCount_;
+
+    std::queue<MYSQL*> connQ_;
+    std::condition_variable cond_;
+    std::mutex mutex_;
+};
+
+class SqlConnPoolGuard{
+public:
+    SqlConnPoolGuard(MYSQL** sql, SqlConnPool* pool){ // 这边 MYSQL 必须 **
+        assert(pool);
+        // 防止资源瞬间抢完（待测试）
+        while (*sql == nullptr){
+            *sql = pool->getMysqlConn();
+        }
+        sql_ = *sql;
+        pool_ = pool;
+    }
+
+    ~SqlConnPoolGuard(){
+        if (sql_){
+//                std::cout << "free" << std::endl;
+            pool_->freeConn(sql_);
+        }
+    }
+private:
+    SqlConnPool *pool_;
+    MYSQL *sql_;
+};
+
+
+#endif //YM_SERVER_SQLCONNPOOL_H
